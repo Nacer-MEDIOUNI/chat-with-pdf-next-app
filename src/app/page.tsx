a43339e52f12f910c4e73472e4541d0fd4451678
Nacer MEDIOUNI <nacer.mediouni@gmail.com>
2024-12-22 20:12:48 +0100

    Setup Neondb and DrizzleORM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,47 @@
 import { Button } from "@/components/ui/button";
+import { auth, UserButton } from "@clerk/nextjs";
+import { ArrowRight, LogIn } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
   return (
-    <div>
-      <div className="flex justify-center">
-        <div className="w-1/2">
-          <Button>Hi there!</Button>
+    <div className="w-screen min-h-screen bg-gradient-to-t from-gray-700 via-gray-900 to-black text-white">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center gap-4 ">
+          <div className="flex flex-col items-center">
+            <UserButton afterSignOutUrl="/" />
+            <h1 className="mr-3 text-5xl font-semibold">Chat with your PDFs</h1>
+            <p className="text-lg">
+              Upload your PDF and start chatting with it
+            </p>
+          </div>
+          <div className="flex flex-col items-center mt-2">
+            {isAuth && (
+              <Button className="rounded-xl border-white border-2">
+                Go to Chats <ArrowRight className="ml-2 " />
+              </Button>
+            )}
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-white">
+            Join millions of students, researchers and professionals to
+            instantly answer questions and understand research with AI
+          </p>{" "}
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <>UploadFile</>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get Started!
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </div>
