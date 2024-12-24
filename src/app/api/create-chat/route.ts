a7224207b6ebda1011e5291f8363acc2b855c721
Nacer MEDIOUNI <nacer.mediouni@gmail.com>
2024-12-24 11:43:23 +0100

    Setup react query and toaster

---
+++ src/app/api/create-chat/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server"
+
+async function POST(req:Request, res:Response) {
+    try {
+        const body= await req.json()
+        const {file_key, file_name} = body
+        console.log(file_key, file_name)
+        return NextResponse.json({ message: "Success" }, { status: 200 }  )
+ 
+
+    } catch (error) {
+        console.log(error)
+    return NextResponse.json({ message: "Error" }, { status: 500 })
+    }
+}
